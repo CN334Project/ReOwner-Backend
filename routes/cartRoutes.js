619af34698dd26deba65b610fb8c55647087c17f
EPNP <88675469+EPNP@users.noreply.github.com>
2024-04-29 00:46:20 +0700

    add function getProductCategories to create

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const cartController = require("../controllers/cartController");
 
 router.post("/add", cartController.addToCart);
-router.patch("/remove", cartController.removeFromCart); 
+router.patch("/remove", cartController.removeFromCart);
 router.get("/", cartController.getCart);
 router.patch("/:id", cartController.updateCartById);
 router.get("/:id", cartController.getCartById);
+router.post("/category", cartController.getProductCategories);
 
 module.exports = router;
